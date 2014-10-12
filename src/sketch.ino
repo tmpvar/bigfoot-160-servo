73202fe33b444777d6ddf46970d03e1e8a2b8f98
Elijah Insua <tmpvar@gmail.com>
2014-10-11 23:44:29 -0700

    sweep back and forth

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,22 @@
+#include <Servo.h>
+
+Servo myservo;
 
 void setup()
 {
+  myservo.attach(9);
 }
 
-void loop()
-{
+int v = 0;
+int dir = 1;
+
+void loop() {
+  if (v>90) {
+    dir = -1;
+  } else if (v < -90) {
+    dir = 1;
+  }
+  v+=dir;
+  myservo.write(90+v);  // set servo to mid-point
+  delay(50);
 }
