bfc16958e5c06616fd422c99ad4a8752dacda29c
Elijah Insua <tmpvar@gmail.com>
2014-10-12 14:12:24 -0700

    hand roll a quadrature decoder @ x4

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,34 +1,61 @@
-#include <Servo.h>
-#define ENCODER_OPTIMIZE_INTERRUPTS
-#include <Encoder.h>
+// #include <Servo.h>
+// Servo myservo;
 
-Servo myservo;
-Encoder myEnc(5, 6);
+int v = 0;
+int dir = 1;
+uint8_t a, b, la, lb;
+
+uint8_t a_mask = (1 << 2);
+uint8_t b_mask = (1 << 3);
+
+// A B
+// 0 0
+// 0 1
+// 1 1
+// 1 0
+
+unsigned long time = 0;
+long position = 0;
+
+uint8_t spots[4] = {0, 1, 3, 2};
+uint8_t spot = 0;
+
 
 void setup()
 {
   // myservo.attach(9);
   Serial.begin(115200);
+  DDRD &= 0xFF ^ (a_mask | b_mask);
+
+  uint8_t val = ((PIND & a_mask) | (PIND & b_mask)) >> 2;
+
+  // figure out where we booted
+  for (uint8_t i = 0; i<4; i++) {
+    if (val == spots[i]) {
+      spot = i;
+      break;
+    }
+  }
 }
 
-int v = 0;
-int dir = 1;
 
-long oldPosition  = 0;
 void loop() {
-  // if (v>5) {
-  //   dir = -1;
-  // } else if (v < -5) {
-  //   dir = 1;
-  // }
-  // v+=dir;
-  // myservo.write(90+v);  // set servo to mid-point
+  uint8_t val = ((PIND & a_mask) | (PIND & b_mask)) >> 2;
+  uint8_t p = spot > 0 ? spot-1 : 3;
+  uint8_t n = spot < 3 ? spot+1 : 0;
 
-  long newPosition = myEnc.read();
-  if (newPosition != oldPosition) {
-    oldPosition = newPosition;
-    Serial.println(newPosition);
+  unsigned long now = millis();
+
+  if (val == spots[p]) {
+    time = now;
+    spot = p;
+    position--;
+  } else if (val == spots[n]) {
+    spot = n;
+    position++;
+    time = now;
+  } else if (now - time > 1000) {
+    Serial.println(position);
+    time = now;
   }
-
-//  delay(50);
 }
